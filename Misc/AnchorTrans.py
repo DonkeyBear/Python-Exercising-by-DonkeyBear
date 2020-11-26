6372391b11bcb65a7f06726c04a97da58e798367
tzouandy2818 <35475647+tzouandy2818@users.noreply.github.com>
2020-11-26 16:49:19 +0800

    Update AnchorTrans.py

--- Misc/AnchorTrans.py
+++ Misc/AnchorTrans.py
@@ -4,5 +4,11 @@ while True:
     a = input('直接輸入 Enter 即可退出程式。\n請輸入標題：')
     if a == '':
         break
-    a = a.lower().split('.')
-    print('輸出結果為：#' + ''.join(a), end='\n\n')
+    '''
+    a = a.lower().split('.') # 轉小寫，並以 . 為分界拆分字串為 list。
+    a = (''.join(a)).split(' ') # 將 list 重新組合後，以空格為分界再次拆分字串為 list。
+    a = '-'.join(a) # 將 list 用 # 重新組合
+    '''
+    # 將以上三句重新嵌套拼裝
+    a = '-'.join((''.join(a.lower().split('.'))).split(' '))
+    print('輸出結果為：#' + a, end='\n\n')

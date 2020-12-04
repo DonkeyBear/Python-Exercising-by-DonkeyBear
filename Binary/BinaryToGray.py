8c1b2c9f119da9b1af1c4b1182b1785671c1964e
tzouandy2818 <35475647+tzouandy2818@users.noreply.github.com>
2020-12-04 10:55:45 +0800

    Update BinaryToGray.py

--- Binary/BinaryToGray.py
+++ Binary/BinaryToGray.py
@@ -10,10 +10,16 @@ for i in range(0, len(binaryList)):
     print('Error')
     break
 
+# 從 LSB 往回作互斥或運算
 for i in range(1, len(binaryList)):
-    if binaryList[:-i] == binaryList[:-i-1]:
+    if binaryList[-i] == binaryList[-i-1]:
         grayList.insert(0, '0')
-    if binaryList[:-i] != binaryList[:-i-1]:
+    if binaryList[-i] != binaryList[-i-1]:
         grayList.insert(0, '1')
 
+# 將二元碼之 MSB 置於格雷碼之 MSB
 grayList.insert(0, binaryList[0])
+
+# 將串列轉換成字串後印出
+grayOutput = ''.join(grayList)
+print(grayOutput)

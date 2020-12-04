257e3eba33d5ac9d44b3fa3ac430e0d7160340ae
tzouandy2818 <35475647+tzouandy2818@users.noreply.github.com>
2020-12-04 17:48:20 +0800

    Update CaesarBrute-force.py

---
+++ Encode and Encrypt/CaesarBrute-force.py
@@ -0,0 +1,5 @@
+from sys import exit as sysExit
+
+upperList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+lowerList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+

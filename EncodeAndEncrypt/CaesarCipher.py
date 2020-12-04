7e66a5e8b5dc72d69c0acffc4682536575165caf
tzouandy2818 <35475647+tzouandy2818@users.noreply.github.com>
2020-12-04 13:37:49 +0800

    Update CaesarCipher.py

--- EncodeAndEncrypt/CaesarCipher.py
+++ EncodeAndEncrypt/CaesarCipher.py
@@ -1,3 +1,19 @@
+from sys import exit as sysExit
+
+modeCheck = input('【凱薩密碼轉換器】\n輸入 1 進入「加密模式」，\n輸入 2 進入「解密模式」：')
+
+if modeCheck == 1:
+    None
+    sysExit()
+
+if modeCheck == 2:
+    None
+    sysExit()
+
+else:
+    print('退出程式……')
+    sysExit()
+
 plainText = input()
 plainList = [] 
 plainList[:0] = plainText

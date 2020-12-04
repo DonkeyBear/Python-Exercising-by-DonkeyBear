6933a3d09ffbbe44494ac50a356d2b8dfb756b23
tzouandy2818 <35475647+tzouandy2818@users.noreply.github.com>
2020-12-04 10:39:56 +0800

    Update BinaryToGray.py

--- Binary/BinaryToGray.py
+++ Binary/BinaryToGray.py
@@ -2,8 +2,14 @@ binaryInput = input()
 binaryList = [] 
 binaryList[:0] = binaryInput
 
+# 檢查輸入是否為二元碼
+'''
 for i in range(0, len(binaryList)):
     if binaryList[i] == '0' or binaryList[i] == '1':
         continue
     print('Error')
     break
+'''
+
+for i in range(1, len(binaryList) + 1):
+    j = -i

c4ca974bafe4df7a648c14d609337f0c9a1f9a05
tzouandy2818 <35475647+tzouandy2818@users.noreply.github.com>
2020-12-04 10:29:04 +0800

    Update BinaryToGray.py

--- Binary/BinaryToGray.py
+++ Binary/BinaryToGray.py
@@ -1,3 +1,9 @@
 binaryInput = input()
 binaryList = [] 
 binaryList[:0] = binaryInput
+
+for i in range(0, len(binaryList)):
+    if binaryList[i] == '0' or binaryList[i] == '1':
+        continue
+    print('Error')
+    break

e44e2d49068bd4bcad5a758b8082562d2f330efe
tzouandy2818 <35475647+tzouandy2818@users.noreply.github.com>
2020-12-07 14:35:52 +0800

    Update VigenereCipher.py

--- Encode and Encrypt/VigenereCipher.py
+++ Encode and Encrypt/VigenereCipher.py
@@ -2,7 +2,6 @@ from sys import exit as sysExit
 
 upperList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 lowerList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-vigenereDict = {'a':0,'b':1,'c':2,'d':3,'e':4,'f':5,'g':6,'h':7,'i':8,'j':9,'k':10,'l':11,'m':12,'n':13,'o':14,'p':15,'q':16,'r':17,'s':18,'t':19,'u':20,'v':21,'w':22,'x':23,'y':24,'z':25}
 
 def encryptVigenere():
     plainText = input('\n請輸入要加密的內容：\n（僅限英文，空白、符號及特殊字元維持原樣不加密）\n')
@@ -18,7 +17,7 @@ def encryptVigenere():
     for i in range(0, len(plainList)):
         if plainList[i].isupper() == True:
             j = upperList.index(plainList[i])
-            j += vigenereDict[keyList[k]]
+            j += lowerList.index(keyList[k])
             if j > 25:
                 j -= 26
             cipherList.append(upperList[j])
@@ -29,7 +28,7 @@ def encryptVigenere():
 
         if plainList[i].islower() == True:
             j = lowerList.index(plainList[i])
-            j += vigenereDict[keyList[k]]
+            j += lowerList.index(keyList[k])
             if j > 25:
                 j -= 26
             cipherList.append(lowerList[j])
@@ -58,7 +57,7 @@ def decryptVigenere():
     for i in range(0, len(cipherList)):
         if cipherList[i].isupper() == True:
             j = upperList.index(cipherList[i])
-            j -= vigenereDict[keyList[k]]
+            j -= lowerList.index(keyList[k])
             plainList.append(upperList[j])
             k += 1
             if k == len(keyList):
@@ -67,7 +66,7 @@ def decryptVigenere():
 
         if cipherList[i].islower() == True:
             j = lowerList.index(cipherList[i])
-            j -= vigenereDict[keyList[k]]
+            j -= lowerList.index(keyList[k])
             plainList.append(lowerList[j])
             k += 1
             if k == len(keyList):

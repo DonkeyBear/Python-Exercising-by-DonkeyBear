46275566004bf28ea1569432df011e664e28a7be
tzouandy2818 <35475647+tzouandy2818@users.noreply.github.com>
2020-12-07 10:57:19 +0800

    Update VigenereCipher.py

--- Encode and Encrypt/VigenereCipher.py
+++ Encode and Encrypt/VigenereCipher.py
@@ -3,12 +3,33 @@ from sys import exit as sysExit
 upperList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 lowerList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
-plainText = input()
-plainText = input('請輸入要加密的內容（僅限英文）：')
-plainList = [] 
-plainList[:] = plainText
-cipherList = []
+def encryptVigenere():
+    plainText = input('請輸入要加密的內容（僅限英文）：')
+    plainList = [] 
+    plainList[:] = plainText
+    cipherList = []
 
-for i in range(0, len(plainList)):
-    if plainList[i] not in upperList and plainList[i] not in lowerList:
-        print('qwdqwdwdqwwqdd')
+    vigenereKey = input('請輸入密鑰（僅限英文）：')
+
+def decryptVigenere():
+    None
+
+#--------------------------------------------------------------------------------
+
+modeCheck = input('【凱薩密碼轉換器】\n輸入 1 進入「加密模式」，\n輸入 2 進入「解密模式」：')
+
+# 加密模式
+if modeCheck == '1':
+    encryptCaesar()
+    input()
+    sysExit()
+
+# 解密模式
+if modeCheck == '2':
+    decryptCaesar()
+    input()
+    sysExit()
+
+else:
+    print('退出程式……')
+    sysExit()

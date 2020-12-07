9ff621a947c317ea205af121d69c70a462f6f140
tzouandy2818 <35475647+tzouandy2818@users.noreply.github.com>
2020-12-07 11:37:40 +0800

    Update VigenereCipher.py

--- Encode and Encrypt/VigenereCipher.py
+++ Encode and Encrypt/VigenereCipher.py
@@ -2,6 +2,7 @@ from sys import exit as sysExit
 
 upperList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 lowerList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+vigenereDict = {'a':1,'b':2,'c':3,'d':4,'e':5,'f':6,'g':7,'h':8,'i':9,'j':10,'k':11,'l':12,'m':13,'n':14,'o':15,'p':16,'q':17,'r':18,'s':19,'t':20,'u':21,'v':22,'w':23,'x':24,'y':25,'z':26}
 
 def encryptVigenere():
     plainText = input('請輸入要加密的內容：\n（僅限英文，空白、符號及特殊字元維持原樣不加密）\n')
@@ -10,16 +11,21 @@ def encryptVigenere():
     cipherList = []
     plainCount = 0
 
-    vigenereKey = input('請輸入密鑰：\n（僅限英文字母，不分大小寫，不可使用空白、符號及其他字元）\n')
+    vigenereKeyO = input('請輸入密鑰：\n（僅限英文字母，不分大小寫，不可使用空白、符號及其他字元）\n').lower
+    vigenereKey = vigenereKeyO
     
-    # 調整密鑰長度以匹配明文長度
+    # 計算明文實際長度
     for i in range(0, len(plainList)):
         if plainList[i] in upperList or plainList[i] in lowerList:
             plainCount += 1
+
+    # 調整密鑰長度以匹配明文長度
     while len(vigenereKey) < plainCount:
-        vigenereKey += vigenereKey
+        vigenereKey += vigenereKeyO
     while len(vigenereKey) > plainCount:
         vigenereKey = vigenereKey[:len(vigenereKey)-1]
+
+    
 
 
 def decryptVigenere():

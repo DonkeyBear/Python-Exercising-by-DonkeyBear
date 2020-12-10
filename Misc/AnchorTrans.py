a069f92c5fc1e13afa18c7a0d9a3835b44652b03
tzouandy2818 <35475647+tzouandy2818@users.noreply.github.com>
2020-12-10 11:03:13 +0800

    引入 pyperclip 自動剪貼的功能

--- Misc/AnchorTrans.py
+++ Misc/AnchorTrans.py
@@ -1,14 +1,16 @@
 # 將 Markdown 中的標題貼上後，自動轉換成符合文法的錨點名稱。
+from pyperclip import copy  as pclipCopy
+from pyperclip import paste as pclipPaste
+
+print('直接按下 Enter 會自動貼上標題，輸入 000 可退出程式。')
 
 while True:
-    a = input('直接輸入 Enter 即可退出程式。\n請輸入標題：')
-    if a == '':
+    a = input('請輸入標題：')
+    if a == '000':
         break
-    '''
-    a = a.lower().split('.') # 轉小寫，並以 . 為分界拆分字串為 list。
-    a = (''.join(a)).split(' ') # 將 list 重新組合後，以空格為分界再次拆分字串為 list。
-    a = '-'.join(a) # 將 list 用 # 重新組合
-    '''
-    # 將以上三句重新嵌套拼裝
-    a = '-'.join((''.join(a.lower().split('.'))).split(' '))
-    print('輸出結果為：#' + a, end='\n\n')
+    if a == '':
+        a = pclipPaste()
+        print(a)
+    a = '#' + '-'.join((''.join(a.lower().split('.'))).split(' '))
+    pclipCopy(a)
+    print('輸出結果為：' + a + '\n　　　　　（已複製到剪貼簿。）\n')
